387ea9dadc59d152c1ec922945fad452bfd453cb
xzz <1183357522@qq.com>
2022-10-28 13:35:40 +0800

    添加模块

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LoginPage></LoginPage>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from "@/components/LoginPage";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginPage
   }
 }
 </script>
---
+++ src/components/LoginPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginPage"
+}
+
+</script>
+
+<style scoped>
+
+</style>
